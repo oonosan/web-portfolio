93278a5b61fddd2b4234f0cb591e622ac8d8cbf7
Karen Ono <onokarent@gmail.com>
2026-04-02 00:07:25 -0300

    Projects component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { AboutMe } from "./about-me/about-me";
 import { TechStack } from "./tech-stack/tech-stack";
 import { Experience } from "./experience/experience";
 import { Contact } from "./contact/contact";
+import { Projects } from './projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [AboutMe, TechStack, Experience, Contact],
+  imports: [AboutMe, TechStack, Experience, Contact, Projects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
