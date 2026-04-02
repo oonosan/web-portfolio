6115c495bcdabd92d184108876f3e7f548c03043
Karen Ono <onokarent@gmail.com>
2026-04-01 23:40:05 -0300

    Tech stack component

--- src/app/tech-stack/tech-stack.ts
+++ src/app/tech-stack/tech-stack.ts
@@ -1,9 +1,30 @@
+import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'tech-stack',
-  imports: [],
   templateUrl: './tech-stack.html',
   styleUrl: './tech-stack.css',
+  standalone: true,
+  imports: [ UpperCasePipe, CommonModule ]
 })
-export class TechStack {}
+export class TechStack {
+  techStacks = [
+    {
+      name: 'Frontend',
+      technologies: ['Angular', 'RxJs', 'Typescript', 'JavaScript', 'HTML', 'CSS', 'SCSS/SASS', 'Bootstrap', 'Material UI', 'Tailwind CSS', 'NgRx', 'Signals', 'Jasmine/Karma'],
+    },
+    {
+      name: 'Backend',
+      technologies: ['NET C#', 'ASP.NET Core', 'Entity Framework Core', 'SQL Server', 'MongoDB', 'SignalR', 'RESTful APIs', 'CQRS', 'MediatR', 'xUnit', 'Moq'],
+    },
+    {
+      name: 'DevOps & Tools',
+      technologies: ['Git', 'GitHub', 'Azure DevOps', 'Azure Cosmos DB'],
+    },
+    {
+      name: 'Development Workflow & Tools',
+      technologies: ['Agile Methodologies', 'Scrum', 'Kanban', 'Jira', 'Visual Studio Code'],
+    }
+  ];
+}
